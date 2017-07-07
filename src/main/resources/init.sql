24a5648094c772564108d907394be7f1f11cd6bb
TDKSS <asasinmax@gmail.com>
2017-07-07 17:48:17 +0300

    added user admin and db_admin

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -4,6 +4,9 @@ USE CAESAR;
 
 
 CREATE USER IF NOT EXISTS 'admin'@'localhost' IDENTIFIED BY '1234%';
+GRANT ALL PRIVILEGES ON *.* TO 'admin'@'localhost';
+
+CREATE USER IF NOT EXISTS 'db_admin'@'localhost' IDENTIFIED BY '1234%';
 GRANT ALL PRIVILEGES ON CAESAR.* TO 'admin'@'localhost';
 
 CREATE USER IF NOT EXISTS 'user_ro'@'localhost' IDENTIFIED BY '1234%';
