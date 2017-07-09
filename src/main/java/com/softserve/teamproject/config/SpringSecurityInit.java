62ba731fb35ec37b2c885f3d771f461a81e3a64b
Анастасія <planet.of.translation@gmail.com>
2017-07-09 18:12:30 +0300

    Created interfaces and services to deal with the security stuff (authorization, log in etc). They include a few methods that are not needed now but will be compulsory when the app has the registration function. Also configured spring security using annotations in config. classes. This code is to be improved after we have DAO layer and controller.

---
+++ src/main/java/com/softserve/teamproject/config/SpringSecurityInit.java
@@ -0,0 +1,7 @@
+package com.softserve.teamproject.config;
+
+import org.springframework.security.web.context
+        .AbstractSecurityWebApplicationInitializer;
+
+public class SpringSecurityInit extends AbstractSecurityWebApplicationInitializer {
+}
--- src/main/java/com/softserve/teamproject/config/WebSecurityConfig.java
+++ src/main/java/com/softserve/teamproject/config/WebSecurityConfig.java
@@ -1,19 +1,65 @@
 package com.softserve.teamproject.config;
 
+import com.softserve.teamproject.service.impl.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao
+        .DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders
+        .AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration
         .EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration
         .WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig  extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/index").permitAll();
+                .antMatchers("/welcome").authenticated()
+                .and()
+                .csrf().disable()
+                .authorizeRequests()
+                .and()
+                .formLogin().loginPage("/signin.jsp").permitAll()
+                .defaultSuccessUrl("/welcome.jsp")
+                .usernameParameter("username")
+                .passwordParameter("password")
+                .failureUrl("/signin-error.jsp")
+                .and()
+                .logout().logoutUrl("/logout").logoutSuccessUrl("/")
+                .and()
+                .rememberMe().key("token").tokenValiditySeconds(3600);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth)
+            throws Exception {
+        auth.authenticationProvider(authenticationProvider());
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider
+                = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
     }
 }
