0b62589e2a72c9e8aa11f610eb2d79cff416b385
Konstantin Musienko <kmusienko97@gmail.com>
2017-07-11 17:19:25 +0300

    Added equals() and hashcode() methods to the each entity (#19)

--- src/main/java/com/softserve/teamproject/entity/Status.java
+++ src/main/java/com/softserve/teamproject/entity/Status.java
@@ -1,5 +1,6 @@
 package com.softserve.teamproject.entity;
 
+import java.util.Objects;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -49,6 +50,27 @@ public class Status {
 
   public void setStatusCategory(StatusCategory statusCategory) {
     this.statusCategory = statusCategory;
+  }
+
+  @Override
+  public boolean equals(Object otherObject) {
+    if (this == otherObject) {
+      return true;
+    }
+    if (otherObject == null) {
+      return false;
+    }
+    if (getClass() != otherObject.getClass()) {
+      return false;
+    }
+    Status other = (Status) otherObject;
+    return Objects.equals(id, other.id) && Objects.equals(name, other.name)
+        && Objects.equals(statusCategory, other.statusCategory);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, name, statusCategory);
   }
 
   @Override
