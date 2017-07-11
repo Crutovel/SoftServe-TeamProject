1683f413eb5617adc7b6c0ed1bde597914538514
tdk <kamenevms96@gmail.com>
2017-07-11 05:35:31 +0300

    before db_script_merge

--- src/main/java/com/softserve/teamproject/config/WebSecurityConfig.java
+++ src/main/java/com/softserve/teamproject/config/WebSecurityConfig.java
@@ -1,6 +1,13 @@
 package com.softserve.teamproject.config;
 
+import com.softserve.teamproject.service.impl.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao
+        .DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders
+        .AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration
         .EnableWebSecurity;
@@ -9,11 +16,59 @@ import org.springframework.security.config.annotation.web.configuration
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig  extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("ram").password("ram123").roles("ADMIN");
+        auth.inMemoryAuthentication().withUser("ravan").password("ravan123").roles("USER");
+        auth.inMemoryAuthentication().withUser("kans").password("kans123").roles("USER");
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/index").permitAll();
+                .antMatchers("/**").authenticated()
+                .and()
+                .csrf().disable()
+                .authorizeRequests()
+                .and()
+               .formLogin()//.loginPage("/signin").permitAll()
+                .defaultSuccessUrl("/welcome")
+                .usernameParameter("username")
+                .passwordParameter("password")
+                .failureUrl("/signin-error")
+                .and()
+                .logout().logoutUrl("/logout").logoutSuccessUrl("/")
+                .and()
+                .rememberMe().key("token").tokenValiditySeconds(3600);
     }
+
+////We don't need a password encoder while we don't have registration
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+
+  /*  @Override
+    protected void configure(AuthenticationManagerBuilder auth)
+            throws Exception {
+        auth.authenticationProvider(authenticationProvider());
+    }
+
+
+/*
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider
+                = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+////We don't need a password encoder while we don't have registration
+//        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }*/
 }
---
+++ src/main/java/com/softserve/teamproject/service/impl/UserDetailsServiceImpl.java
@@ -0,0 +1,53 @@
+package com.softserve.teamproject.service.impl;
+
+import com.softserve.teamproject.entity.Role;
+import com.softserve.teamproject.entity.User;
+import com.softserve.teamproject.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public void setUserRepository(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public UserDetailsServiceImpl() {
+    }
+
+    /**
+     * This is an obligatory method that implements the similar method of the
+     * UserDetailsService interface. Here we get a user from the repository by
+     * user's name, then get his role and then we return a value of UserDetails type,
+     * which is actually provided by the framework itself. It's basically the
+     * procedure of user authorization.
+     *
+     * @param userName
+     * @return
+     * @throws UsernameNotFoundException
+     */
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws
+            UsernameNotFoundException {
+        User user = userRepository.getUserByNickName(userName);
+
+        Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
+        Role role = user.getRole();
+        grantedAuthorities.add(new SimpleGrantedAuthority(role.getName()));
+
+        return new org.springframework.security.core.userdetails.User(user
+                .getNickName(), user.getPassword(), grantedAuthorities);
+    }
+}
