7417e86e8c017bab9191b688551726791727a25f
Crutovel <crutovel@gmail.com>
2017-07-12 02:34:36 +0300

    forgot about init.sql

---
+++ src/main/resources/init.sql
@@ -0,0 +1,11 @@
+DROP SCHEMA IF EXISTS caesar;
+CREATE SCHEMA caesar CHARACTER SET UTF8;
+USE caesar;
+
+CREATE USER IF NOT EXISTS 'db_admin'@'localhost' IDENTIFIED BY 'rNT5Vn[j(>R2r6.UXcJwtv];`RWewYH`';
+GRANT ALL PRIVILEGES ON caesar.* TO 'db_admin'@'localhost';
+
+CREATE USER IF NOT EXISTS 'user_ro'@'localhost' IDENTIFIED BY '12345qwerty';
+GRANT SELECT ON caesar.* TO 'user_ro'@'localhost';
+
+FLUSH PRIVILEGES ;
