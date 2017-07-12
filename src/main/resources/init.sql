91ba580c4b27dc136569ecaeb27298d7113ccd0d
tdk <kamenevms96@gmail.com>
2017-07-12 12:36:01 +0300

    Renamed files and let spring boot do all the dirty job with table creating and inserting

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -1,12 +1,12 @@
-DROP SCHEMA IF EXISTS CAESAR;
-CREATE SCHEMA CAESAR CHARACTER SET UTF8;
-USE CAESAR;
+DROP SCHEMA IF EXISTS caesar;
+CREATE SCHEMA caesar CHARACTER SET UTF8;
+USE caesar;
 
 CREATE USER IF NOT EXISTS 'db_admin'@'localhost' IDENTIFIED BY 'rNT5Vn[j(>R2r6.UXcJwtv];`RWewYH`';
-GRANT ALL PRIVILEGES ON CAESAR.* TO 'admin'@'localhost';
+GRANT ALL PRIVILEGES ON caesar.* TO 'db_admin'@'localhost';
 
 CREATE USER IF NOT EXISTS 'user_ro'@'localhost' IDENTIFIED BY '12345qwerty';
-GRANT SELECT ON CAESAR.* TO 'user_ro'@'localhost';
+GRANT SELECT ON caesar.* TO 'user_ro'@'localhost';
 
 FLUSH PRIVILEGES ;
 
