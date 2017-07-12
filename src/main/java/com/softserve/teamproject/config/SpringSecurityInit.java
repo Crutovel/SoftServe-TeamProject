fa153e3b76f957c28a3b7e634795f1f2b5c85720
Crutovel <crutovel@gmail.com>
2017-07-12 16:53:29 +0300

    javadoc return

--- src/main/java/com/softserve/teamproject/config/SpringSecurityInit.java
+++ src/main/java/com/softserve/teamproject/config/SpringSecurityInit.java
@@ -2,6 +2,11 @@ package com.softserve.teamproject.config;
 
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
+/**
+ * The class extends AbstractSecurityWebApplicationInitializer class that helps us map the
+ * springSecurityFilterChain.
+ */
+
 public class SpringSecurityInit extends AbstractSecurityWebApplicationInitializer {
 
 }
