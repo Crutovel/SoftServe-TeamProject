4e17739316f341e69e6f675ca1e84bdd2290ae31
cdc895642 <cdc895642@hotmail.com>
2017-07-17 16:14:05 +0300

    get list of groups in current user location

--- src/main/java/com/softserve/teamproject/controller/UserProfileController.java
+++ src/main/java/com/softserve/teamproject/controller/UserProfileController.java
@@ -1,8 +1,10 @@
 package com.softserve.teamproject.controller;
 
+import com.softserve.teamproject.entity.Group;
 import com.softserve.teamproject.entity.User;
 import com.softserve.teamproject.service.UserProfileService;
 import java.security.Principal;
+import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,7 +12,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * Controller that used for handle user profile.
+ * Controller that used for handle user profile and related to user information
  * User photo is converted into JSON format as a Base64 string
  */
 @RestController
@@ -33,5 +35,16 @@ public class UserProfileController {
   @ResponseBody
   public User getUserProfile(Principal principal) {
     return userProfileService.getUserProfile(principal.getName());
+  }
+
+  /**
+   * Get groups from user location
+   * @param principal authorized user
+   * @return groups in user location
+   */
+  @RequestMapping(value = "/user/location/groups", method = RequestMethod.GET)
+  @ResponseBody
+  public Set<Group> getGroupsFromUserLocation(Principal principal) {
+    return userProfileService.getGroupsFromUserLocation(principal.getName());
   }
 }
--- src/main/java/com/softserve/teamproject/service/impl/UserProfileServiceImpl.java
+++ src/main/java/com/softserve/teamproject/service/impl/UserProfileServiceImpl.java
@@ -1,8 +1,12 @@
 package com.softserve.teamproject.service.impl;
 
+import com.softserve.teamproject.entity.Group;
+import com.softserve.teamproject.entity.Location;
 import com.softserve.teamproject.entity.User;
+import com.softserve.teamproject.repository.LocationRepository;
 import com.softserve.teamproject.repository.UserRepository;
 import com.softserve.teamproject.service.UserProfileService;
+import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,14 +14,27 @@ import org.springframework.stereotype.Service;
 public class UserProfileServiceImpl implements UserProfileService {
 
   private UserRepository userRepository;
+  private LocationRepository locationRepository;
 
   @Autowired
   public void setUserRepository(UserRepository userRepository) {
     this.userRepository = userRepository;
   }
 
+  @Autowired
+  public void setLocationRepository(LocationRepository locationRepository) {
+    this.locationRepository = locationRepository;
+  }
+
   @Override
   public User getUserProfile(String principalName) {
     return userRepository.getUserByNickName(principalName);
+  }
+
+  @Override
+  public Set<Group> getGroupsFromUserLocation(String principalName) {
+    User currentUser=userRepository.getUserByNickName(principalName);
+    Location userLocation=locationRepository.findOne(currentUser.getLocation().getId());
+    return userLocation.getGroups();
   }
 }
