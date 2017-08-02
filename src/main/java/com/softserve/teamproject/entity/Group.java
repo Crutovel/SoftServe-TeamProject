a2e3ea31a523e1ae0182fa79e7bec4bd79f4f749
Kostya Musienko <kmusienko97@gmail.com>
2017-08-02 15:51:14 +0300

    Added handling ConstraintViolationException

--- src/main/java/com/softserve/teamproject/entity/Group.java
+++ src/main/java/com/softserve/teamproject/entity/Group.java
@@ -27,7 +27,6 @@ import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
-import org.hibernate.validator.constraints.Length;
 import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
