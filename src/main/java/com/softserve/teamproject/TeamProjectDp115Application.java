6300b3f60b5b58377464ed593d2cd3eabd2c6ce1
Crutovel <crutovel@gmail.com>
2017-08-16 15:21:13 +0300

    validation properties fix

--- src/main/java/com/softserve/teamproject/TeamProjectDp115Application.java
+++ src/main/java/com/softserve/teamproject/TeamProjectDp115Application.java
@@ -4,6 +4,7 @@ import javax.validation.Validator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
@@ -21,17 +22,19 @@ public class TeamProjectDp115Application {
    * Need Spring validator instead of hibernate validator for custom constraint annotations.
    */
   @Bean
-  public Validator validator() {
-    return new LocalValidatorFactoryBean();
+  public Validator validator(MessageSource messageSource) {
+    LocalValidatorFactoryBean factory = new LocalValidatorFactoryBean();
+    factory.setValidationMessageSource(messageSource);
+    return factory;
   }
 
   /**
    * Set MethodValidationPostProcessor with Spring validator for custom constraint annotations.
    */
   @Bean
-  public MethodValidationPostProcessor methodValidationPostProcessor() {
+  public MethodValidationPostProcessor methodValidationPostProcessor(MessageSource messageSource) {
     MethodValidationPostProcessor methodValidationPostProcessor = new MethodValidationPostProcessor();
-    methodValidationPostProcessor.setValidator(validator());
+    methodValidationPostProcessor.setValidator(validator(messageSource));
     return methodValidationPostProcessor;
   }
 }
