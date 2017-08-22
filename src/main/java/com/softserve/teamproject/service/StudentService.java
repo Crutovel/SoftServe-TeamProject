625994df1779aaf7793aa4b8dc4f019e7bcff353
AnastasiaBlack <planet.of.translation@gmail.com>
2017-08-22 22:41:44 +0300

    Added a modal window with a form to edit a student.

--- src/main/java/com/softserve/teamproject/service/StudentService.java
+++ src/main/java/com/softserve/teamproject/service/StudentService.java
@@ -9,28 +9,28 @@ import org.springframework.security.access.prepost.PreAuthorize;
 
 public interface StudentService {
 
-  @PreAuthorize("hasAnyAuthority('teacher','coordinator', 'admin')")
+  @PreAuthorize("hasAnyAuthority('teacher','coordinator', 'admin', 'tes')")
   Iterable<StudentResource> getStudentsByGroupId(Integer groupId);
 
-  @PreAuthorize("hasAnyAuthority('teacher','coordinator', 'admin')")
+  @PreAuthorize("hasAnyAuthority('teacher','coordinator', 'admin','tes')")
   Iterable<StudentResource> getAllStudents();
 
-  @PreAuthorize("hasAnyAuthority('coordinator', 'admin')")
+  @PreAuthorize("hasAnyAuthority('coordinator', 'admin', 'tes')")
   Iterable<StudentResource> addStudents(@Valid List<Student> students, Integer groupId, String userName);
 
-  @PreAuthorize("hasAnyAuthority('coordinator', 'admin')")
+  @PreAuthorize("hasAnyAuthority('coordinator', 'admin', 'tes')")
   Iterable<StudentResource> updateStudents(@Valid List<Student> students, String userName);
 
-  @PreAuthorize("hasAnyAuthority('coordinator', 'admin')")
+  @PreAuthorize("hasAnyAuthority('coordinator', 'admin', 'tes')")
   Student updateSingleStudent(Student student, String userName);
 
-  @PreAuthorize("hasAnyAuthority('teacher', 'coordinator', 'admin')")
+  @PreAuthorize("hasAnyAuthority('teacher', 'coordinator', 'admin', 'tes')")
   StudentResource getStudentResourceById(Integer id);
 
-  @PreAuthorize("hasAnyAuthority('teacher', 'coordinator', 'admin')")
+  @PreAuthorize("hasAnyAuthority('teacher', 'coordinator', 'admin', 'tes')")
   StudentResource updateStudent(@Valid Student student);
 
-  @PreAuthorize("hasAnyAuthority('coordinator', 'admin')")
+  @PreAuthorize("hasAnyAuthority('coordinator', 'admin', 'tes')")
   public List<StudentDto> getAllStudentDto();
 
 }
